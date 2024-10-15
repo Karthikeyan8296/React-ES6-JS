29ca5fbe28f4044955211b6c7606420df32192f8
Karthikeyan <165583967+Karthikeyan8296@users.noreply.github.com>
2024-10-15 19:45:02 +0530

    ES6 - Javascript

---
+++ src/main.jsx
@@ -0,0 +1,44 @@
+//Here we will write codes//
+
+function sayHello() {
+  for (let i = 0; i <= 5; i++) {
+    console.log(i);
+    // use let only when we need to reasign the variable
+  }
+  let x = 1;
+  x = 10;
+  console.log(x);
+}
+sayHello();
+
+//OBJECTS//
+const person = {
+  name: "karthik",
+  //this is method//
+  walk: function () {
+    return "20";
+  },
+  // from es6 we can use like this
+  talk() {},
+};
+person.name = "john";
+console.log(person.name);
+
+//THIS KEYWORD//
+const human = {
+  name: "karthik",
+  draw() {
+    console.log(this);
+  },
+};
+human.draw();
+
+//ARROW FUNTIONS//
+//normal function
+const number = function (number) {
+  return number * number;
+};
+console.log(number(10));
+//arrow function
+const randomNumber = (n) => n * n;
+console.log(randomNumber(2));
